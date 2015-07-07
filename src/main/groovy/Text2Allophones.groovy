4aafdbb01f5348e0763c1e2cd11de24eebbfec19
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-07-07 15:59:30 +0200

    update main groovy class to actually do the conversion

--- src/main/groovy/Text2Allophones.groovy
+++ src/main/groovy/Text2Allophones.groovy
@@ -1,14 +1,34 @@
 import marytts.LocalMaryInterface
 import marytts.Version
+import groovy.xml.XmlUtil
 
 class Text2Allophones {
 
     static void main(args) {
-        def localeStr = args.first()
+        def (localeStr, textDirPath, xmlDirPath) = args.take(3)
+
+        // init mary
         def locale = Locale.forLanguageTag(localeStr == 'en' ? 'en-US' : localeStr)
         def mary = new LocalMaryInterface()
         mary.locale = locale
+        mary.outputType = 'ALLOPHONES'
         println "MaryTTS ${Version.specificationVersion()}, ${mary.locale.displayLanguage}"
+
+        // init input, output directories, parser
+        def textDir = new File(textDirPath)
+        def xmlDir = new File(xmlDirPath)
+        def parser = new XmlSlurper(false, false)
+
+        // process
+        textDir.eachFile { inputFile ->
+            println "Synthesizing $inputFile.name"
+            def doc = mary.generateXML(inputFile.text)
+            def xmlStr = XmlUtil.serialize(doc.documentElement)
+            def xml = parser.parseText(xmlStr)
+            def outputFile = new File(xmlDir, inputFile.name - 'txt' + 'xml')
+            outputFile.text = XmlUtil.serialize(xml)
+            println "Wrote $outputFile.name"
+        }
     }
 
 }
